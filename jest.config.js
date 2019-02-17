5fe792eee5a730623c99e5e1295fb2de411175a4
tnagorra <weathermist@gmail.com>
2019-02-18 00:38:30 +0545

    Change jest to include all files for coverage

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
 module.exports = {
+    roots: [
+        '<rootDir>/src',
+    ],
+    collectCoverageFrom: [
+        '**/*.{js,jsx}',
+        '!**/node_modules/**',
+    ],
     transform: {
         '^.+\\.jsx?$': './babel-transform.js',
     },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
     moduleFileExtensions: [
         'js',
         'jsx',
-    ],
-    moduleDirectories: [
-        'node_modules',
-        'src',
     ],
 };
